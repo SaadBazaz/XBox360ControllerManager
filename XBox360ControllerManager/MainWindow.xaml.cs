893c2b1d4e147c6ab8c3fe438704047c2b840275
Saad Bazaz <saadbazaz@hotmail.com>
2021-02-16 21:25:13 +0500

    Windows Game Bar can be controlled but only when a game opens

--- XBox360ControllerManager/MainWindow.xaml.cs
+++ XBox360ControllerManager/MainWindow.xaml.cs
@@ -8,9 +8,15 @@ using ContextMenu = System.Windows.Forms.ContextMenu;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
 using MenuItem = System.Windows.Forms.MenuItem;
 
+using System;
+using System.Windows.Forms;
+using System;
+using System.Runtime.InteropServices;
 
 using WindowsInput;
 using WindowsInput.Native;
+
+
 
 
 namespace XBox360ControllerManager
@@ -18,8 +24,19 @@ namespace XBox360ControllerManager
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    [Windows.Foundation.Metadata.Static(typeof(Windows.Gaming.UI.GameBar), 131072, "Windows.Foundation.UniversalApiContract")]
     public partial class MainWindow
     {
+
+        bool GameBarVisible = false;
+
+
+
+        private const int WM_SETTEXT = 0x000c;
+
+        private const int WM_KEYDOWN = 0x0100;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -43,28 +60,158 @@ namespace XBox360ControllerManager
             };
             notifyIcon.DoubleClick += (sender, args) => Show();
         }
-
-        private void InitializeXBox360Timer()
+        internal static class NativeMethods
         {
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+            [return: MarshalAs(UnmanagedType.Bool)]
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool PostMessage(IntPtr hWnd, int Msg, Keys wParam, IntPtr lParam);
+
+            [DllImport("User32.dll")]
+            public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+        }
+    private void InitializeXBox360Timer()
+        {
+                    //Listen to GameBar visibility status
+                    Windows.Gaming.UI.GameBar.VisibilityChanged +=
+                (s, e) => {
+                    GameBarVisible = !GameBarVisible;
+                };
+            System.Diagnostics.Debug.WriteLine("This is a log");
             Timer timer = new Timer { Enabled = true };
             int guideCounter = 0;
             timer.Tick += (sender, args) =>
             {
                 if (AnyGamepadHasButton(XBox360GamepadButton.Guide) && !IsVisible)
                 {
+
+                    System.Diagnostics.Debug.WriteLine(GameBarVisible);
+                    System.Diagnostics.Debug.WriteLine(Windows.Gaming.UI.GameBar.IsInputRedirected);
+                    System.Diagnostics.Debug.WriteLine(guideCounter);
+
                     guideCounter++;
                     if (guideCounter == 1)
                     {
                         var sim = new InputSimulator();
                         sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_G);
-                    }   
+                        //if (Windows.Gaming.UI.GameBar.Visible)
+                        //{
+                        //    //the game bar is visible;
+                        //}
+
+                        //if (Windows.Gaming.UI.GameBar.IsInputRedirected)
+                        //{
+                        //    //the game bar has input;
+                        //}
+                    }
                     else if (guideCounter == 10)
                     {
                         Show();
                     }
+                    //else if (guideCounter > 10)
+                    //{
+                    //    guideCounter = 0;
+                    //}
                 }
+                else 
+                //if (true)
+                //{
+
+                    if (AnyGamepadHasButton(XBox360GamepadButton.A))
+                    {
+
+                        var sim = new InputSimulator();
+                        sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                        //IntPtr hWndNotepad = NativeMethods.FindWindow("Xbox Game Bar", null);
+
+                        //IntPtr hWndEdit = NativeMethods.FindWindowEx(hWndNotepad, IntPtr.Zero, "Edit", null);
+
+                        ////NativeMethods.SendMessage(hWndEdit, WM_SETTEXT, 0, " key");
+
+                        //NativeMethods.PostMessage(hWndEdit, WM_KEYDOWN, Keys.Tab, IntPtr.Zero);
+
+                        //Console.ReadKey(true);
+
+                    }
+                    else if (AnyGamepadHasButton(XBox360GamepadButton.Y))
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.KeyPress(VirtualKeyCode.SPACE);
+                        //IntPtr hWndNotepad = NativeMethods.FindWindow("Xbox Game Bar", null);
+
+                        //IntPtr hWndEdit = NativeMethods.FindWindowEx(hWndNotepad, IntPtr.Zero, "Edit", null);
+
+                        ////NativeMethods.SendMessage(hWndEdit, WM_SETTEXT, 0, " key");
+
+                        //NativeMethods.PostMessage(hWndEdit, WM_KEYDOWN, Keys.Tab, IntPtr.Zero);
+
+                        //Console.ReadKey(true);
+
+                    }
+                    else if (AnyGamepadHasButton(XBox360GamepadButton.DPadRight))
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.KeyPress(VirtualKeyCode.RIGHT);
+                        //SendKeys.SendWait("{TAB}");
+                    }
+                    else if (AnyGamepadHasButton(XBox360GamepadButton.DPadLeft))
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.KeyPress(VirtualKeyCode.LEFT);
+
+                    }
+                    else if (AnyGamepadHasButton(XBox360GamepadButton.DPadDown))
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.KeyPress(VirtualKeyCode.DOWN);
+
+                    }
+                    else if (AnyGamepadHasButton(XBox360GamepadButton.DPadUp))
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.KeyPress(VirtualKeyCode.UP);
+
+                    }
+                    else if (AnyGamepadHasButton(XBox360GamepadButton.RightShoulder))
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.KeyPress(VirtualKeyCode.TAB);
+
+                    }
+                    else if (AnyGamepadHasButton(XBox360GamepadButton.LeftShoulder))
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.SHIFT, VirtualKeyCode.TAB);
+                    }
+                    else if (AnyGamepadHasButton(XBox360GamepadButton.Back))
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.CONTROL, VirtualKeyCode.SHIFT }, VirtualKeyCode.TAB);
+                    }
+
+                    else if (AnyGamepadHasButton(XBox360GamepadButton.Start))
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.TAB);
+                    }
+                    else if (AnyGamepadHasButton(XBox360GamepadButton.B))
+                    {
+                        System.Diagnostics.Debug.WriteLine(Windows.Gaming.UI.GameBar.Visible);
+                        System.Diagnostics.Debug.WriteLine(Windows.Gaming.UI.GameBar.IsInputRedirected);
+                        var sim = new InputSimulator();
+                        sim.Keyboard.KeyPress(VirtualKeyCode.ESCAPE);
+                    }
+                //}
+
                 else if (AnyGamepadHasButton(XBox360GamepadButton.B) && IsVisible)
                 {
+                    var sim = new InputSimulator();
+                    sim.Keyboard.KeyPress(VirtualKeyCode.ESCAPE);
                     Hide();
                 }
                 else if (AnyGamepadHasButton(XBox360GamepadButton.X) && IsVisible)
